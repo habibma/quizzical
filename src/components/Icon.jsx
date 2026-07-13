94c2964f0c1cba2965ef7bb134ff28a87f4aee5a
habibma <habib.motavassel@gmail.com>
2026-07-13 17:39:47 +0200

    chor: jsx file

--- src/components/Icon.jsx
+++
@@ -1,7 +0,0 @@
-import './Icon.css';
-
-const Icon = ( { src, alt = "", size = 24, className = "" } ) => (
-  <img src={src} alt={alt} width={size} height={size} className={ `icon ${className}` } />
-);
-
-export default Icon;
