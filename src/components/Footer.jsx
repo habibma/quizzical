3acc090bbcb046aed657b4dcccfb62d7b8b5f0e1
habibma <habib.motavassel@gmail.com>
2026-07-13 10:20:03 +0200

    ui: footer prettier

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
+import './Footer.css'
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div>
-        <small>&copy; 2023 Quizzical. All rights reserved. | </small>
-        <small>Coded by <a className='link' href='https://habibmote.com/' target='_blank'>Habib Mote</a></small>
+        <small>&copy; 2023 Quizzical | All rights reserveds | </small>
+        <small className="small">Coded by <a className='link' href='https://habibmote.com/' target='_blank'>Habib Mote</a></small>
       </div>
     </footer>
   )
