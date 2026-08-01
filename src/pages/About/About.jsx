a76a0692b8d7752066f208411a1bca3f542a774e
habib motavassel <habib.motavassel@gmail.com>
2026-08-01 21:08:24 +0200

    doc: to add a proper description

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,15 +1,12 @@
-import Footer from "../../components/Footer"
-import Navbar from "../../components/Navbar"
+import './About.css';
 
 const About = () => {
     return (
         <div className="container">
-            <div className="about-container">
-                <h1>About Quizzical</h1>
-                <p>Quizzical is a fun and interactive quiz application that allows users to test their knowledge on various topics. It provides a user-friendly interface and a wide range of questions to challenge your brain.</p>
-                <p>Whether you're a trivia enthusiast or just looking for a fun way to pass the time, Quizzical has something for everyone. Start playing today and see how many questions you can answer correctly!</p>
+            <div className="content">
+                <h1 className="about-title">About Quizzical</h1>
+                <p className="about-description">Quizzical is a quiz platform that enables teachers, educators, and parents to create, customize, and manage quizzes, while giving learners an engaging way to test and expand their knowledge.</p>
             </div>
-            <Footer />
         </div>
     )
 }
