50b5091d166c32f4212c3e78fe01e87f3dbcdd86
habib motavassel <habib.motavassel@gmail.com>
2026-08-17 20:00:22 +0200

    chore: clean code

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -3,18 +3,13 @@ import { getCategories as getTriviaAPICategories } from './triviaAPIService.js';
 
 export async function getCategories(repository) {
 
-    console.log("repository: ", repository);
     const endpoint = repository.capabilities.find(cap => cap.name.toLowerCase() === "categories");
-
-    console.log(endpoint);
 
     const apiConfig = {
         baseUrl: repository.baseUrl,
         endpoint: endpoint.path,
         method: endpoint.method,
     };
-
-    console.log(apiConfig);
 
     switch (repository.adaptor) {
         case 'opentdb':
