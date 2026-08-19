b70f78603673e8ef2fe0b6edcf36f0f1731c1a22
habib motavassel <habib.motavassel@gmail.com>
2026-08-19 23:22:23 +0200

    UX: to show a msg when there is no categories

--- src/pages/Admin/Repositories/CategoryModal.jsx
+++ src/pages/Admin/Repositories/CategoryModal.jsx
@@ -7,28 +7,30 @@ const CategoryModal = ({ isOpen, onClose, list, repoId, onToggleCategory }) => {
   return (
     <Modal customClass="repos-category-modal" isOpen={isOpen} onClose={onClose}>
       <h3>Categories</h3>
-      <table className="category-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Status</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-        {list.map(item => (
-          <tr key={item.id}>
-            <td>{item.displayName}</td>
-            <td>{item.enabled ? "Enabled" : "Disabled"}</td>
-            <td>
-              <button onClick={() => onToggleCategory(repoId, item.id)}>
-                {item.enabled ? "Disable" : "Enable"}
-              </button>
-            </td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
+      {list.length > 0 ? (
+        <table className="category-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map(item => (
+              <tr key={item.id}>
+                <td>{item.displayName}</td>
+                <td>{item.enabled ? "Enabled" : "Disabled"}</td>
+                <td>
+                  <button onClick={() => onToggleCategory(repoId, item.id)}>
+                    {item.enabled ? "Disable" : "Enable"}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        ) : (<p>No categories available.</p>)}
     </Modal>
   )
 }
